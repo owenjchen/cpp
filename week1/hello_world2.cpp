5790e0b9c91d10d536180ac5b95659687dcd545c
Owen Chen <owenjchen@gmail.com>
2023-07-13 23:01:58 -0400

    week1

---
+++ week1/hello_world2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello, everyone!\nGreeting from C++ Class.\n" ;
+}
---
+++ week2/int_max_numbers.cpp
@@ -0,0 +1,35 @@
+#include <cstdint>
+#include <climits>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Max numbers from <climits>
+    int n = INT_MAX;
+    unsigned un = UINT_MAX;
+    long l = LONG_MAX;
+    cout << "Max int = " << n << endl;
+    cout << "Max unsigned int = " << un << endl;
+    cout << "Max long = " << l << endl;    
+
+    // Fixed width integers and max macros in <cstdint>
+    int8_t  int8 = INT8_MAX;
+    int16_t int16 = INT16_MAX;
+    int32_t int32 = INT32_MAX; 
+    int64_t int64 = INT64_MAX;
+    cout << "Max int8 = " << INT8_MAX << endl;
+    cout << "Max int16 = " << int16 << endl;   
+    cout << "Max int32 = " << int32 << endl;  
+    cout << "Max int64 = " << int64 << endl;  
+
+    // Calculated limits;
+    int n2 = (long)(pow(2, 31) - 1);
+    unsigned un2 = (long)(pow(2, 32) - 1);
+    long l2 = (long)(pow(2, 63) - 1);
+
+    cout << "Max int = " << n2 << endl;
+    cout << "Max unsigned int = " << un2 << endl;
+    cout << "Max long = " << l2 << endl;      
+}
